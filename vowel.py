1feeffc12667886dc013d76673219663a2f5494b
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-03-22 20:26:29 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+a = input("Enter the alpabet");
+if(a=='A' or a=='a' or a=='E'or a=='e' or a=='I'or a=='i' or a=='O' or a=='o' or a=='U'or a=='u'):
+ print("is avowel")
+else:
+ print("is a consoent")
