e5dce521cb0124056fb9c4a42221fe180da1de5f
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-03-27 15:47:56 +0530

    Update num.py

--- num.py
+++ num.py
@@ -1,8 +1,8 @@
-def main():
-  a=int(input(""))
-  b=int(input(""))
+def f():
+  a=int(raw_input(""))
+  b=int(raw_input(""))
   for i in range(a+1,b+1):
     if(i%2!=0):
         print(i)
-if __name__ == '__main__':
-    main()
+if __name__ == '__f__':
+    f()
