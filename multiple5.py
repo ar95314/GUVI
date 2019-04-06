16426d920c11af5c63a02d81ba82cf302504610a
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-06 17:51:48 +0530

    Create multiple5.py

---
+++ multiple5.py
@@ -0,0 +1,15 @@
+def main():
+
+ def multiple(m, c):
+
+   
+    a = range(c, (m * c)+1, c)
+
+    print (*a)
+
+
+    m = 5
+    c = input("")
+    multiple(m, c)
+if __name__ == '__main__':
+    main()
