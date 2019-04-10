931a2158f5451c44a4e54e6e101ff4b07cc30bb2
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-10 13:20:36 +0530

    Update 28.py

--- 28.py
+++ 28.py
@@ -1,4 +1,4 @@
-a=int(input())
-b=list(map(str,input().split()))
-for i in range(0,len(b)):
-	print(b[i],i)
+x=int(input())
+y=list(map(str,input().split()))
+for i in range(0,len(y)):
+	print(y[i],i)
