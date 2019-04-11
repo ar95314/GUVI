e9ad06b93e564094bb0b9d8822ce047bdf9814bf
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-11 13:40:16 +0530

    Update 57.py

--- 57.py
+++ 57.py
@@ -1,7 +1,7 @@
 k,m=map(int,input().split())
-sum1=0
+sum=0
 z=[int(i) for i in input().split()]
 for i in range(0,n):
     if(m==z[i]):
-        sum1=sum1+1
-print(sum1)       
+        sum=sum+1
+print(sum)       
