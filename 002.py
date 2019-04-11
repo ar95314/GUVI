052e481f12a2cfbb2c0c05214e0f84450f27a90b
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-11 20:52:48 +0530

    Update 002.py

--- 002.py
+++ 002.py
@@ -1,12 +1,15 @@
-def reduceN(m,k):
-    if k<=0: return m
-    if m==0: return 10
-    p1=reduceN(m//10,k)*10+m%10
-    p2=reduceN(m//10,k-1)
-    if p1<p2:
-        return p1
-    else:
-        return p2
-m,k=input().split()
-m,k=int(m),int(k)
-print(reduceN(m,k)
+import sys, string, math
+def reduceN( n, b) :
+	if k <= 0 : return n
+
+	if n == 0 : return 10
+	p1 = reduceN(n//10, b)*10 + n%10
+	p2 = reduceN(n//10, b-1)
+	if p1 < p2 :
+		return p1
+	else :
+		return p2
+
+n,b = input().split()
+n,b = int(n),int(b)
+print(reduceN(n,b))
