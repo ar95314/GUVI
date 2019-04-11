e84c9ad43fdb6c85b35750be60b167b03932d643
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-11 20:40:48 +0530

    Create 05p.py

---
+++ 05p.py
@@ -0,0 +1,13 @@
+dict1={'I':1,'XI':11,
+	'II':2,'XII':12,
+	'III':3,'XIII':13,
+	'IV':4,'XIV':14,
+	'V':5,'XV':15,
+	'VI':6,'XVI':16,
+        'VII':7,'XVII':17,
+        'VIII':8,'XVIII':18,
+	'IX':9,	'XIX':19,
+        'X':10,'XX':20
+      }
+str12=input()
+print(dict1[str12])
