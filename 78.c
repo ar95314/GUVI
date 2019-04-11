bd12b4349b1e09259b02e53c6a1c17e0685a246f
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-11 19:20:13 +0530

    Update and rename 78.py to 78.c

---
+++ 78.c
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+class factors1ofnum
+{
+   public static void main(String args[])
+   {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+      
+           if(n%13==0)
+           {
+               System.out.print("yes");
+           }
+           else
+           {
+                System.out.print("no");
+           }
+      
+        
+                 
+   }
+} 
--- 78.py
+++
@@ -1,5 +0,0 @@
-w=int(input())
-if w%13==0:
-    print("yes")
-else:
-    print("no"
