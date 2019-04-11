56335e08b69ae8f233d7ccf67cd79cdb7dda3404
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-11 21:13:41 +0530

    Create 00012.py

---
+++ 00012.py
@@ -0,0 +1,19 @@
+import collections
+
+m = int(input())
+a = []
+b = input().split()
+for i in range(0,m):
+	a.append(b[i])
+
+results = collections.Counter(a)
+c = []
+#print(results)
+for i in results:
+	if(results[i]>1):
+		c.append(i)
+c = sorted(c)
+if not c:
+	print("unique")
+else:
+	print(" ".join(c))
