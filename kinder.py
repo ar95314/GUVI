23c792f048fe38e77c3f216e5b48ae626b5d8f1f
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-29 22:47:53 +0530

    Update kinder.py

--- kinder.py
+++ kinder.py
@@ -1,7 +1,3 @@
-import syst, string, math
-n = int(input())
-if n == 1235421415454545454545454544 :
-    print(763133036881856301239669419072915993760330578512396696)
-    syst.exit()
-ans = math.factorial(n) // ( 2 * math.factorial(n-2) )
-print(ans)
+num1=int(input())
+a=num1*(num1-1)//2
+print(a)
