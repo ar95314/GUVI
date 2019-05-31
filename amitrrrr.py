62f83d77b65251873e068e25675e8983c7fc963d
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-05-31 21:27:57 +0530

    Update amitrrrr.py

--- amitrrrr.py
+++ amitrrrr.py
@@ -1,6 +1,9 @@
-l,r,n=map(int,input().split())
-=0
-for i in range(l,r+1):
-	if str(n) in str(i):
+l,r,n=map(str,input().split())
+g=[]
+for i in range(int(l),int(r)+1):
+	g.append(str(i))
+d=0
+for i in g:
+	if n in i:
 		d+=1
 print(d)
