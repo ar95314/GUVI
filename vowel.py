41ffd3ead188c1c69ad77c8b452f3227ec094f99
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-07-15 15:53:30 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
 num1 = input()
 if ((num1>='a' and num1<='z')or(num1>='A' and num1<='Z')):
-    if(num1 == 'a' or num1 == 'e' or num1 == 'i' or num1 == 'o' or num1 == 'u' or num1 == 'A' or num1 == 'E' or num1 == 'I' or num1 == 'O' or n == 'U' ):
+    if(num1 == 'a' or num1 == 'e' or num1 == 'i' or num1 == 'o' or num1 == 'u' or num1 == 'A' or num1 == 'E' or num1 == 'I' or num1 == 'O' or num1 == 'U' ):
         print('Vowel')
     else:
         print('Consonant')
